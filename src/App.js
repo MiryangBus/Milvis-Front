9476fae66cff7d723dd17aae9c2054e831685c41
Suyeon <msjgr2801@gmail.com>
2022-09-26 16:43:58 +0900

    components & pages setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Header from "./components/Header";
+import Main from "./page/Main";
+import Train from "./page/Train"
+import Map from "./page/Map";
+import Bus from "./page/Bus";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Main />}/>
+          <Route exact path="/train" element={<Train />}/>
+          <Route exact path="/map" element={<Map />}/>
+          <Route exact path="/bus" element={<Bus />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
