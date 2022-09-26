a7e1380254cb5ae8d5dccb7e363942e10942c491
Suyeon <msjgr2801@gmail.com>
2022-09-26 22:49:11 +0900

    edit button component & set slide

--- src/page/Main.js
+++ src/page/Main.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import FooterMain from '../components/Footer-main'
+import Slide from '../components/Slide'
 
 const Main = () => {
   return (
     <>
       <main>
-        
+        <Slide></Slide>
       </main>
       <FooterMain />
     </>
