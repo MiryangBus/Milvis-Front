17542b6a790cfd4f2401ceab9f599a615c141112
hyunah9 <hyn2626@naver.com>
2022-09-27 16:51:12 +0900

    bustimetable#1

---
+++ src/components/Footer-bus.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Footer-main.css";
+
+const FooterBus = () => {
+  return (
+    <footer>
+      <span>역-학교 | 학교-역 </span>
+    </footer>
+
+  )
+}
+
+export default FooterBus
--- src/page/Bus.js
+++ src/page/Bus.js
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import "./Bus.css";
+import FooterBus from "../components/Footer-bus";
 const Bus = () =>  {
+
   return (
-    <div>Bus</div>
+    <bus-date>
+      <div>평일</div>
+      <div>주말</div>
+      <div>대학만방학</div>
+      <div>모두방학</div>
+    </bus-date>
+    
   )
 }
 
 export default Bus
+
