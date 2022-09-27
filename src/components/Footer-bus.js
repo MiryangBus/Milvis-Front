84aeeaf9cbd35b2f484e9d9d1a7e260e3f2f8bac
hyunah9 <hyn2626@naver.com>
2022-09-27 23:24:07 +0900

    BusTimeTable#1

--- src/components/Footer-bus.js
+++ src/components/Footer-bus.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import "./Footer-main.css";
-
+//Footer-main = Footer-main, Footer-bus 이므로 Footer.css로 합치고
+//안에 class로 쪼개기.
+//수연이는 .안씀 나는 .쓰고 className으로
 const FooterBus = () => {
   return (
-    <footer>
-      <span>역-학교 | 학교-역 </span>
-    </footer>
+    <div className='footer-bus'>
+      <div >밀양역-&gt;학교 </div> {/*div span text차이? */}
+      <div className='footer-space'>학교-&gt;밀양역 </div>
+    </div>
 
   )
 }
--- src/page/Bus.js
+++ src/page/Bus.js
@@ -2,14 +2,16 @@ import React from 'react'
 import "./Bus.css";
 import FooterBus from "../components/Footer-bus";
 const Bus = () =>  {
-
   return (
-    <bus-date>
-      <div>평일</div>
-      <div>주말</div>
-      <div>대학만방학</div>
-      <div>모두방학</div>
-    </bus-date>
+  <>
+    <div className="bus-date">
+      <text>평일</text>
+      <text className="bus-date-space">주말</text>
+      <text className="bus-date-space">대학만 방학</text>  
+      <text className="bus-date-space">모두 방학</text> {/*한줄 안띄우는건 div아닌 text*/}
+    </div>
+    <FooterBus></FooterBus>
+  </>
     
   )
 }
