e1d5a0385722aa4cd7ab4483a558091a844dbd44
hyunah9 <hyn2626@naver.com>
2022-09-28 13:42:09 +0900

    버스시간표 기능 임시

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import "./Header.css";
 import {List} from 'react-bootstrap-icons';
-
+import {Link} from 'react-router-dom'
 const Header = () => {
   return (
     <header>
-      <div className="title-container">
-        <h1>Milvis</h1>
+      <div className="title-container" >
+        <Link to = "/" style={{ textDecoration: 'none' , color:"white"}}>
+            <h1>Milvis</h1>
+        </Link>
       </div>
-      <div className="navbar-container">
-        <List/>
-      </div>
+
+      <Link to = "/sidebar" style={{ textDecoration: 'none' , color:"white"}}>
+        <div className="navbar-container">
+          <List width={"2rem"} height={"2rem"}/>
+        </div>
+      </Link>
+
     </header>
   )
 }
-
 export default Header
