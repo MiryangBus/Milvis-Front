985bede3273b3188c306c01be1d8125b5fc7e7d6
hyunah9 <hyn2626@naver.com>
2022-09-28 17:56:50 +0900

    20220928-1

--- src/components/Footer-bus.js
+++ src/components/Footer-bus.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import "./Footer-main.css";
+import Nav from 'react-bootstrap/Nav';
 
 //Footer-main = Footer-main, Footer-bus 이므로 Footer.css로 합치고
 //안에 class로 쪼개기.
 //수연이는 .안씀 나는 .쓰고 className으로
 const FooterBus = () => {
   return (
-    <div className='footer-bus'>
-      <div >밀양역-&gt;학교 </div> {/*div span text차이? */}
-      <div className='footer-space'>학교-&gt;밀양역 </div>
-    </div>
+    <>
+      <Nav className='footer-bus' variant="pills" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </>
 
   )
 }
 
 export default FooterBus
+
+
+
+// <div >밀양역-&gt;학교 </div> {/*div span text차이? */}
+// <div className='footer-space'>학교-&gt;밀양역 </div>
--- src/page/Bus.js
+++ src/page/Bus.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
 import "./Bus.css";
 import FooterBus from "../components/Footer-bus";
 import {Truck} from 'react-bootstrap-icons' ;
+
+
+
+const BusTimeTable = [
+  {ID:1, hour:6, goto:'station', time1:'6시 05분',time2:'6시 05분',time3:'6시 05분'},
+  {ID:2, hour:6, goto:'station', time1:'6시 10분',time2:'6시 05분',time3:'6시 05분'},
+  {ID:3, hour:6, goto:'station', time1:'6시 34분',time2:'6시 05분',time3:'6시 05분'},
+  {ID:4, hour:6, goto:'station', time1:'6시 55분',time2:'6시 05분',time3:'6시 05분'},
+  {ID:5, hour:7, goto:'station', time1:'7시 05분',time2:'7시 05분',time3:'7시 05분'},
+  {ID:6, hour:7, goto:'station', time1:'7시 35분',time2:'6시 05분',time3:'6시 05분'},
+];
+
+const timeList = BusTimeTable.map(bustime=> <li key={bustime.id}>{bustime.time1}</li>)
 
 const Bus = () =>  {
   return (
@@ -15,19 +28,24 @@ const Bus = () =>  {
     <div className="bus-content">
       <div className = "bus-stop">
         <text>밀양캠</text> {/* font 정해서 일괄 적용하기 */}
-        <text className="bus-stop-space">영남루</text>
         <text className="bus-stop-space">밀양역</text>
+        <text className="bus-stop-space">영남루</text>
       </div>
       <Truck front size="3em" width="7em" color="black"/>
+      <ul>{timeList}</ul>
+
+
     </div>
-
-
-
-
-
     <FooterBus></FooterBus>
   </>
 
   )
 }
 export default Bus
+
+
+
+
+
+
+
