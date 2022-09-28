6a3fa4def36c31518f8fa8ef69aeae401c451e16
Suyeon <msjgr2801@gmail.com>
2022-09-28 10:48:38 +0900

    Revert "sidebar pull"
    This reverts commit 29a1d3db959138a34bcd89ffb05e3a1ea01bd34d, reversing
    changes made to be5176d749d2864a8a47ed93e752b05534e42112.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Main from "./page/Main";
 import Train from "./page/Train"
 import Map from "./page/Map";
 import Bus from "./page/Bus";
-import SideBar from "./page/SideBar";
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
           <Route exact path="/train" element={<Train />}/>
           <Route exact path="/map" element={<Map />}/>
           <Route exact path="/bus" element={<Bus />}/>
-          <Route exact path="/sidebar" element={<SideBar />}/>
         </Routes>
       </Router>
     </div>
--- src/page/Bus.js
+++ src/page/Bus.js
@@ -1,18 +1,9 @@
 import React from 'react'
-import "./Bus.css";
-import FooterBus from "../components/Footer-bus";
-const Bus = () =>  {
 
+const Bus = () =>  {
   return (
-    <bus-date>
-      <div>평일</div>
-      <div>주말</div>
-      <div>대학만방학</div>
-      <div>모두방학</div>
-    </bus-date>
-    
+    <div>Bus</div>
   )
 }
 
 export default Bus
-
