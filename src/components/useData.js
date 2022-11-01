11d68dd830722e1c5e2afd09be10febb60930e72
Suyeon <msjgr2801@gmail.com>
2022-11-01 15:12:55 +0900

    Feat: POST, GET 메소드 추가
    - fetch 함수로 데이터 불러오고 보내는 메소드 추가
    - 1. callData (GET 메소드)
    - 2. sendData (POST 메소드)
    - 외부 API 불러올 시 promise 처리가 어떻게 될지는 한 번 받아와봐야 알 수 있습니다

---
+++ src/components/useData.js
@@ -0,0 +1,29 @@
+// 1. 데이터 받아오기 (get)
+export const callData = async(url) => {
+  try {
+    const data = await fetch(url);
+    const res = data.json();
+    
+    return res;
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+// 2. 데이터 보내기 (post)
+export const sendData = async(url) => {
+  // fetch option object
+  const requestOption = {
+    method: 'POST'
+  }
+  
+  try {
+    const data = await fetch(url, requestOption);
+    const res = data.json();
+    
+    return res;
+  } catch(err) {
+    console.log(err);
+  }
+
+}
