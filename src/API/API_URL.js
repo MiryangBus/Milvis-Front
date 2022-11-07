678a4eee13b553a795557986ee73be34d49e8419
Suyeon <msjgr2801@gmail.com>
2022-11-07 19:11:53 +0900

    Remove: [API_URL.js]

--- src/API/API_URL.js
+++
@@ -1,3 +0,0 @@
-export const TIME_TABLE_URL = `http://13.209.94.41/search_time_table`;
-export const TIME_TABLE_ORIGIN = `http://13.209.94.41`;
-export const MAP_URL = `http://13.209.94.41/path_finding`
