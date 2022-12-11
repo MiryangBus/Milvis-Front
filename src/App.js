28fd7305989521a32a752dc1d98d6c76c753758d
Suyeon <msjgr2801@gmail.com>
2022-12-11 16:19:35 +0900

    Fix: polyline css 충돌 문제 해결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import "./App.css";
 import Header from "./components/common/Header";
 import Main from "./pages/Main";
 import Train from "./pages/train/Train"
@@ -7,19 +8,21 @@ import TrainResult from "./pages/train/TrainResult";
 import Map from "./pages/Map";
 import Bus from "./pages/Bus";
 import MapResult from "./pages/MapResult";
-import "./App.css";
+import PolyLine from "./pages/PolyLine";
+
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Header />
+        {/* <Header /> */}
         <Routes>
           <Route exact path="/" element={<Main />}/>
           <Route exact path="/train" element={<Train />}/>
           <Route exact path="/train/time-table" element={<TrainResult />} />
           <Route exact path="/map" element={<Map />}/>
           <Route exact path="/map-result/:lat/:lng" element={<MapResult />} />
+          <Route exact path="/map-result/test" element={<PolyLine />}/>
           <Route exact path="/bus/:date" element={<Bus />}/>
         </Routes>
       </Router>
