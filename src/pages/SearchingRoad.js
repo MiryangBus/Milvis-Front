c95accb7eadbfcc3aaea6728a842b981391123a7
hyunah9 <hyn2626@naver.com>
2022-12-19 23:07:10 +0900

    stations정보확인

--- src/pages/SearchingRoad.js
+++ src/pages/SearchingRoad.js
@@ -11,15 +11,17 @@ import { TIME_TABLE_ORIGIN, MAP_URL } from "../API/API_URL";
 // test페이지 만듦
 const SearchingRoad = (props) => {
   const { lat, lng, showCate } = useParams();
-  const [lat4, setLat4] = useState(33.45103658098629);
-  const [lng4, setLng4] = useState(126.57130488606091);
-  const [lat2, setLat2] = useState(33.45326416780721);
-  const [lng2, setLng2] = useState(126.57148752901962);
-  const [lat3, setLat3] = useState(33.45322480184726);
-  const [lng3, setLng3] = useState(126.5731441083554);
+  const [lat4, setLat4] = useState(35.450180777031726);
+  const [lng4, setLng4] = useState(128.79987274871453);
+  const [lat2, setLat2] = useState(35.45120243188731);
+  const [lng2, setLng2] = useState(128.79723027759243);
+  const [lat3, setLat3] = useState(35.45201469173419);
+  const [lng3, setLng3] = useState( 128.79714922310873);
   const [state, setState] = useState({data : []})
-//  const tttt={one:[{"name":"x","name2":"y"}]}
-//  console.log(tttt.one[0].name)
+
+
+  //좌표(polyline찍을거(목적지도 포함됨))
+
   const onSubmit = async(e) => {
     // e.preventDefault();
     const data = {};
@@ -29,34 +31,39 @@ const SearchingRoad = (props) => {
     console.log(showCate)
     data.is_depart_from_campus = showCate
     const res = await sendData(MAP_URL, JSON.stringify(data));
-    setState({ data: res})
-    console.log(state.data.results[0].stations);//0번 노선
+    setState({ data: res.results})
   };
+
+
 
 
   useEffect(()=>{
   const mapContainer = document.getElementById('map'), // 지도를 표시할 div  
     mapOption = { 
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        center: new kakao.maps.LatLng(35.45373762287106, 128.806692348998), // 지도의 중심좌표
         level: 4 // 지도의 확대 레벨
     };
 
   const map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
   const positions = [
+    // state.data.map((road, index) => (
+    //   {
+    //     latlng:new kakao.maps.LatLng(road.stations.x, road.stations.y)
+    //   }
+    // ))
     {
-        title: '출발지', 
-        latlng: new kakao.maps.LatLng(lat, lng)
+        latlng: new kakao.maps.LatLng(35.450180777031726, 128.79987274871453)
     },
     {
-        title: '꺾이는지점', 
-        latlng: new kakao.maps.LatLng(lat2, lng2)
+        latlng: new kakao.maps.LatLng(35.45120243188731 , 128.79723027759243)
     },
     {
-        title: '도착지', 
-        latlng: new kakao.maps.LatLng(lat3, lng3)
+        latlng: new kakao.maps.LatLng(35.45201469173419 , 128.79714922310873)
     },
 
 ];
+
+
 
 // 마커 이미지의 이미지 주소입니다
 const imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
@@ -78,6 +85,7 @@ for (var i = 0; i < positions.length; i ++) {
     });
     }
     // 선을 구성하는 좌표 배열입니다. 이 좌표들을 이어서 선을 표시합니다
+
     const linePath = [
         new kakao.maps.LatLng(lat4, lng4),
         new kakao.maps.LatLng(lat2, lng2),
@@ -87,7 +95,7 @@ for (var i = 0; i < positions.length; i ++) {
     // 지도에 표시할 선을 생성합니다
     const polyline = new kakao.maps.Polyline({
         path: linePath, // 선을 구성하는 좌표배열 입니다
-        strokeWeight: 10, // 선의 두께 입니다
+        strokeWeight: 9, // 선의 두께 입니다
         strokeColor: '#FFAE00', // 선의 색깔입니다
         strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
         strokeStyle: 'solid' // 선의 스타일입니다
@@ -99,14 +107,24 @@ for (var i = 0; i < positions.length; i ++) {
 
   return (
     <div>
-           
       <div className='map-explain'>test후 데이터 받고 결과 <br />보기 </div>
       <div id="map" style={{width:"350px", height:"700px"}}></div> 
       <Button onClick={onSubmit} className="map-button" variant="primary" >
           test
       </Button>
+      {
+      state.data ? (
+        state.data.map((road, index) => (
+          console.log(road.stations)
+        )
+        )
+      ): ("no")
+      }
     </div>
   )
+
 }
 
 export default SearchingRoad
+// console.log(state.data.results[0].stations);//0번 노선
+//          console.log(road.stations.length)
