ff8e628d059d45401979cf95ed2ef7bcb0c16670
hyunah9 <hyn2626@naver.com>
2023-01-06 20:28:32 +0900

    commit

--- src/pages/SearchingRoad.js
+++ src/pages/SearchingRoad.js
@@ -17,8 +17,9 @@ const SearchingRoad = (props) => {
   const [state, setState] = useState({data : []})
 
   const onSubmit = async(e) => {
-    // e.preventDefault();
+    // e.preventDefault(); 
     const data = {};
+   
     data.depart_time = "2022-12-11T12:11:00";
     data.station_x = lat;
     data.station_y = lng;
@@ -26,69 +27,74 @@ const SearchingRoad = (props) => {
     const res = await sendData(MAP_URL, JSON.stringify(data));
     setState({ data: res.results}) 
   };
+  onSubmit()
 
   useEffect(()=>{
-    onSubmit()
-    const countLine = [];
-    const positions = [];
-    state.data.forEach((road) => {
-      const {stations} = road;  //road.stations를 stations의 배열로 만듦 .
-      countLine.push(road.line_id)
-      console.log(countLine)
-      stations.forEach((station) => {
-        const prop = {};
-        const value = new kakao.maps.LatLng(station.x, station.y);
-        prop.latlng = value;
-        positions.push(prop);
-        console.log(prop) 
-      })
-    })
+ 
+},[]);   
+const countLine = [];
+const positions = [];
+console.log(state.data)
 
-    const mapContainer = document.getElementById('map'), // 지도를 표시할 div  
-    mapOption = {  
-        center: new kakao.maps.LatLng(35.45373762287106, 128.806692348998), // 지도의 중심좌표
-        level: 4 // 지도의 확대 레벨
-    };
-    const map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+state.data.forEach((road) => { 
+  const {stations} = road;  //road.stations를 stations의 배열로 만듦 .
+  countLine.push(road.line_id)
+  console.log(countLine,countLine.length)
+  stations.forEach((station) => {
+    const prop = {};
+    const value = new kakao.maps.LatLng(station.x, station.y);
+    prop.latlng = value;
+    positions.push(prop);
+    console.log(prop) 
+  })
+})
+
+const mapContainer = document.getElementById('map'), // 지도를 표시할 div  
+mapOption = {  
+    center: new kakao.maps.LatLng(35.45373762287106, 128.806692348998), // 지도의 중심좌표
+    level: 4 // 지도의 확대 레벨
+};
+const map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
 
 // 마커 이미지의 이미지 주소입니다
-  const imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
-  for (var i = 0; i < positions.length; i ++) {
-      
-      // 마커 이미지의 이미지 크기 입니다
-      const imageSize = new kakao.maps.Size(24, 35); 
-      
-      // 마커 이미지를 생성합니다    
-      const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
-      
-      // 마커를 생성합니다
-      const marker = new kakao.maps.Marker({
-          map: map, // 마커를 표시할 지도
-          position: positions[i].latlng, // 마커를 표시할 위치
-          title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-          image : markerImage // 마커 이미지 
-      });
-  }
-    // 선을 구성하는 좌표 배열입니다. 이 좌표들을 이어서 선을 표시합니다
-  const linePath = [
-      new kakao.maps.LatLng(35.450180777031726, 128.79987274871453),
-      new kakao.maps.LatLng(35.45120243188731 , 128.79723027759243),
-      new kakao.maps.LatLng(35.45201469173419 , 128.79714922310873) 
-  ];
-
-    // 지도에 표시할 선을 생성합니다
-    const polyline = new kakao.maps.Polyline({
-        path: linePath, // 선을 구성하는 좌표배열 입니다
-        strokeWeight: 9, // 선의 두께 입니다
-        strokeColor: '#FFAE00', // 선의 색깔입니다
-        strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-        strokeStyle: 'solid' // 선의 스타일입니다
-    });
-    // 지도에 선을 표시합니다 
-    polyline.setMap(map);  
-},[]);   
+const imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
+for (var i = 0; i < positions.length; i ++) {
   
+  // 마커 이미지의 이미지 크기 입니다
+  const imageSize = new kakao.maps.Size(24, 35); 
+  
+  // 마커 이미지를 생성합니다    
+  const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
+  
+  // 마커를 생성합니다
+  const marker = new kakao.maps.Marker({
+      map: map, // 마커를 표시할 지도
+      position: positions[i].latlng, // 마커를 표시할 위치
+      title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+      image : markerImage // 마커 이미지 
+  });
+}
+// 선을 구성하는 좌표 배열입니다. 이 좌표들을 이어서 선을 표시합니다
+const linePath = [
+  new kakao.maps.LatLng(35.450180777031726, 128.79987274871453),
+  new kakao.maps.LatLng(35.45120243188731 , 128.79723027759243),
+  new kakao.maps.LatLng(35.45201469173419 , 128.79714922310873) 
+];
+
+// 지도에 표시할 선을 생성합니다
+const polyline = new kakao.maps.Polyline({
+    path: linePath, // 선을 구성하는 좌표배열 입니다
+    strokeWeight: 9, // 선의 두께 입니다
+    strokeColor: '#FFAE00', // 선의 색깔입니다
+    strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+    strokeStyle: 'solid' // 선의 스타일입니다
+});
+// 지도에 선을 표시합니다 
+polyline.setMap(map);    
+
+
+
   return (
     <div>
       <div className='map-explain'>test후 데이터 받고 결과 <br />보기 </div>
